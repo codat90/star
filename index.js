170b0ac2fb688ed8f097cb968d0e769188bd1d4d
Stefan S. Adrian <40149299+codat90@users.noreply.github.com>
2022-07-24 18:37:30 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+$(window).on("load",function(){
+	$(".loader-wrapper").fadeOut("slow");
+});
+
+document.onreadystatechange = function () {
+	if(document.readyState === 'complete') {
+		document.body.style.overflow = 'visible';
+	}
+}
+
+// PAGE SCROLL //
+function scrollTopAnimated() {
+	$("html, body").animate(
+		{scrollTop: $("#longpage").offset().top}, 1000);
+}
+
+$(window).on("load",function(){
+	setTimeout(scrollTopAnimated, 1000);
+});
+
